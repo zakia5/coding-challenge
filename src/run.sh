58cf7dfab2bb6a05e7f1a1efb4ab9c35943b0ff1
zakia5 <zakia555@yahoo.com>
2016-04-08 19:17:11 -0400

    third commit

--- src/run.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# Shell script to execute programs, with the input directory tweet_input and output the files in the directory tweet_output
-python3 ./src/tweets_cleaned.py ./tweet_input/tweets.txt ./tweet_output/ft1.txt
-python3 ./src/average_degree.py ./tweet_input/tweets.txt ./tweet_output/ft2.txt
-
-
-
